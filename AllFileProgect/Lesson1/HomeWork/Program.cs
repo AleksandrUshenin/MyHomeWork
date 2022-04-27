d7f08c28e2f01743b6bf1286f9c9599c4042d3df
AleksandrUshenin <sasha0625@mail.ru>
2022-04-27 22:12:29 +0300

    added all files lesson1

---
+++ AllFileProgect/Lesson1/HomeWork/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя: ");
+            string UserName = Console.ReadLine();
+            DateTime MyData = new DateTime();
+            MyData = DateTime.Now;
+            Console.WriteLine("Привет, {0}, сегодня {1}.", UserName, MyData.ToShortDateString());   // получам только дату
+
+            Console.ReadLine();
+        }
+    }
+}
